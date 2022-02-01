9dbb5d26d00481cccb21f90b2321e8452ccafae8
daniel <danielpalma232@gmail.com>
2022-02-01 15:53:57 -0300

    Arquivos modificados para o projeto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+
+import Aulas from "./components/Aulas";
+import Home from "./components/Home";
+import Nav from "./components/Nav";
+import Sobre from "./components/Sobre";
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route path="/aulas" element={<Aulas />}></Route>
+          <Route path="/sobre" element={<Sobre />}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
