be505b96e848822044ed9f327c70c9ade2910264
daniel <danielpalma232@gmail.com>
2022-02-03 16:49:05 -0300

    Acrescentado descrição das aulas

--- src/Data.js
+++ src/Data.js
@@ -1,7 +1,7 @@
 const aulas = [
-  {id: 0, title: 'Aula Inicial', desc: ""},
-  {id: 1, title: 'Como Configurar', desc: ""},
-  {id: 2, title: 'Hello World', desc: ""}
+  {id: 0, title: 'Aula Inicial', desc: "Primeira aula do Semestre"},
+  {id: 1, title: 'Como Configurar', desc: "Ensinando a como condigurar sua máquina"},
+  {id: 2, title: 'Hello World', desc: "Primeiro Hello World"}
 ]
 
 export default aulas;
